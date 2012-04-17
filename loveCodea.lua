b48eb7c557bf43acda6d0721fcdc2da0090838d4
Mr Speaker <mrspeaker@gmail.com>
2012-04-17 16:52:22 +0200

    fix for deltaX,deltaY. Use class def from the binary

--- loveCodea.lua
+++ loveCodea.lua
@@ -146,8 +146,8 @@ CurrentTouch.x = 0
 CurrentTouch.y = 0
 CurrentTouch.prevX = 0
 CurrentTouch.prevY = 0
---  CurrentTouch.deltaX
---  CurrentTouch.deltaY
+CurrentTouch.deltaX = 0
+CurrentTouch.deltaY = 0
 CurrentTouch.id = 0
 CurrentTouch.state = ENDED
 CurrentTouch.tapCount = 0
@@ -168,51 +168,52 @@ lineCapsMode = ROUND
 -- Ellipse Modes
 ellipMode = CENTER
 
--- class.lua
+-- Class.lua
 -- Compatible with Lua 5.1 (not 5.0).
-function class(base, init)
-	local c = {}    -- a new class instance
-	if not init and type(base) == 'function' then
-		init = base
-		base = nil
-	elseif type(base) == 'table' then
-		-- our new class is a shallow copy of the base class!
-		for i,v in pairs(base) do
-			c[i] = v
-		end
-		c._base = base
-	end
-	-- the class will be the metatable for all its objects,
-	-- and they will look up their methods in it.
-	c.__index = c
+function class(base)
+    local c = {}    -- a new class instance
+    if type(base) == 'table' then
+        -- our new class is a shallow copy of the base class!
+        for i,v in pairs(base) do
+            c[i] = v
+        end
+        c._base = base
+    end
 
-	-- expose a constructor which can be called by <classname>(<args>)
-	local mt = {}
-	mt.__call = function(class_tbl, ...)
-		local obj = {}
-		setmetatable(obj,c)
-		if class_tbl.init then
-			class_tbl.init(obj,...)
-		else
-			-- make sure that any stuff from the base class is initialized!
-			if base and base.init then
-				base.init(obj, ...)
-			end
-		end
-		return obj
-	end
-	c.init = init
-	c.is_a = function(self, klass)
-		local m = getmetatable(self)
-		while m do
-			if m == klass then return true end
-			m = m._base
-		end
-		return false
-	end
-	setmetatable(c, mt)
-	return c
+    -- the class will be the metatable for all its objects,
+    -- and they will look up their methods in it.
+    c.__index = c
+
+    -- expose a constructor which can be called by <classname>(<args>)
+    local mt = {}
+    mt.__call = function(class_tbl, ...)
+        local obj = {}
+        setmetatable(obj,c)
+        if class_tbl.init then
+            class_tbl.init(obj,...)
+        else 
+            -- make sure that any stuff from the base class is initialized!
+            if base and base.init then
+                base.init(obj, ...)
+            end
+        end
+        
+        return obj
+    end
+
+    c.is_a = function(self, klass)
+        local m = getmetatable(self)
+        while m do 
+            if m == klass then return true end
+            m = m._base
+        end
+        return false
+    end
+
+    setmetatable(c, mt)
+    return c
 end
+
 
 -------------------
 -- Graphics
@@ -567,6 +568,8 @@ function love.update(dt)
 
 	-- use Mouse for Touch interaction
 	local touch_changed = false
+    local prevX = CurrentTouch.prevX
+    local prevY = CurrentTouch.prevY
 	if love.mouse.isDown("l") then
 		-- get Mouse position as Touch position
 		-- publish globally
@@ -606,6 +609,8 @@ function love.update(dt)
 		local touch = {}
 		touch.x = CurrentTouch.x
 		touch.y = CurrentTouch.y
+        touch.deltaX = touch.x - prevX
+        touch.deltaY = touch.y - prevY
 		touch.state = CurrentTouch.state
 		touch.id = 1 -- TODO: What does ID this mean?
 		touched(touch)
